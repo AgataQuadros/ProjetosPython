a02dda338eb13b64c873ea28f1ca10ca367863ba
AgataQuadros <quadrosagata@gmail.com>
2024-04-16 19:55:37 -0300

    Ficha de dados

---
+++ aula003_entrada_de_dados/entrada_de_dados.py
@@ -0,0 +1,27 @@
+# import
+import os
+
+
+# limpar terminal
+os.system('cls')
+
+print('-' * 70)
+print('ENTRADA DE DADOS EM PYTHON')
+print('=' * 70)
+
+# entrada
+nome = input('Entre com seu nome: ')
+nascimento = input('Data de nascimento: ')
+peso = input('Entre com o peso: ')
+altura = input('Entre com sua altura: ')
+
+# saida
+print('-' * 70)
+print('SAIDA DE DADOS')
+print('=' * 70)
+print('Nome:', nome)
+print('Data de nascimento:', nascimento)
+print('Peso:', peso)
+print('Altura:', altura)
+print('-' * 70)
+print('')
