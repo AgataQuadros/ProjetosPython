f4aba75771b3af10dd867dec126bd77321f03f65
AgataQuadros <quadrosagata@gmail.com>
2024-04-15 21:42:36 -0300

    ficha

---
+++ aula002_variaveis/variaveis.py
@@ -0,0 +1,32 @@
+# importando da biblioteca de sistema
+import os
+
+
+# limpando o terminal
+os.system('cls')
+
+print('-' * 70)
+print('Estudo de variáveis')
+print('='*70) # firula
+
+# As variaveis são declaradas por infeência
+nome = 'Jhon Doe'
+nascimento = 1970
+altura = 1.80
+peso = 75.5
+doador = True
+complexo = 3j # Python tabalha diretamente com numeros comlexos
+PI = 3.14 # isso é uma CONSTANTE, seu valor não deve ser alterado
+
+# Saída utilizando o método type() para verificar o tipo da variável
+print('\033[0m \033[31mTipos declarados: \033[0m')
+print('\033[0m \033[32m nome \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m nascimento \033[0m é do tipo : ', type(nascimento))
+print('\033[0m \033[32m altura \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m peso \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m doador \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m complexo \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m PI \033[0m é do tipo : ', type(nome))
+print('\033[0m \033[32m nome \033[0m é do tipo : ', type(nome))
+print('-'*70)
+print('')
