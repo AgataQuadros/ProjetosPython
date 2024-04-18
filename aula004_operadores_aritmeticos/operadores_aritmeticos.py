fa3d08558b3fce2103f3c86bcf3cb9967e90ba23
AgataQuadros <quadrosagata@gmail.com>
2024-04-17 21:39:41 -0300

    operadores aritméticos

---
+++ aula004_operadores_aritmeticos/operadores_aritmeticos.py
@@ -0,0 +1,36 @@
+# import
+import os
+
+# Limpar terminal 
+os.system('cls')
+
+print('-' * 70)
+print('OPERADORES ARITMÉTICOS')
+print('-' * 70)
+
+# Entrada de dados
+print('--------- SOMA')
+print('-' * 70)
+parcela_1 = float(input('Entre com a 1ª parcela'))
+parcela_2 = float(input('Entre com a 2ª parcela'))
+
+print()
+print('---------- SUBITRAÇÃO')
+print('-' * 70)
+minuendo = float(input('Encontre o minunendo'))
+subtraendo = float(input('Encontre o subtraendo'))
+
+print()
+print('---------- PRODUTO')
+print('-' * 70)
+multiplicando = float(input('Entre com o multiplicando'))
+multiplicador = float(input('Entre com o multiplicador'))
+
+print()
+print('--------- DIVISÃO')
+print('-' * 70)
+
+
+
+
+
