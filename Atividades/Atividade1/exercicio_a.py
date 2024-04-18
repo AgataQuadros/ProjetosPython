c4592203cb795b7d3adae5df97d761e764bec744
AgataQuadros <quadrosagata@gmail.com>
2024-04-18 20:48:40 -0300

    Exercício A

---
+++ Atividades/Atividade1/exercicio_a.py
@@ -0,0 +1,32 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('Exercicio A')
+print('-' * 20)
+
+# Entrada
+valor1 = float(input('Digite um numero: '))
+valor2 = float(input('Digite um numero: '))
+valor3 = float(input('Digite um numero: '))
+
+# Processamento
+soma = valor1 + valor2 + valor3
+multiplicacao = valor1 * valor2 * valor3
+
+# Saída
+print('')
+print('-' * 20)
+print('RESULTADOS')
+print('-' * 20)
+print(f'A sua soma dos valores {valor1} + {valor2} + {valor3} resulta em {soma}')
+print(f'A sua multiplicação dos valores {valor1} x {valor2} x {valor3} result em {multiplicacao}')
+print('=' * 50)
