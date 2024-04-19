d26b4ed0a8492cfad972ad53c8090425dde0035b
AgataQuadros <quadrosagata@gmail.com>
2024-04-18 21:04:58 -0300

    Exercício B

---
+++ Atividades/Atividade1/exercicio_b.py
@@ -0,0 +1,23 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+# Limpar o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCICIO B')
+print('-' * 20)
+
+
+
+
+
+
+
+
+
