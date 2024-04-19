17202ae9da1864251feac3ae732988332a36d1f8
AgataQuadros <quadrosagata@gmail.com>
2024-04-19 19:59:08 -0300

    Exicício J mandar

---
+++ Atividades/Atividade1/exercicio_j.py
@@ -0,0 +1,29 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO J')
+print('-' * 20)
+
+# Etrada
+lado1 = (float(input('Entre com o lado 1: ')))
+lado2 = (float(input('Entre com o lado 2: ')))
+
+# Processamento
+resultado = (lado1 * 2) + (lado2 * 2)
+
+# Saída
+print('')
+print('-' * 20)
+print(f'O perímetro do retangulo é {resultado}')
+print('-' * 20)
+print('')
+print('=' * 50)
