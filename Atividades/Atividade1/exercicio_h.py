337ade29216e01dbb62058c7d8fd7df26d9f894e
AgataQuadros <quadrosagata@gmail.com>
2024-04-19 20:58:09 -0300

    Exercício H

---
+++ Atividades/Atividade1/exercicio_h.py
@@ -0,0 +1,41 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO H')
+print('-' * 20)
+
+# Biblioteca
+import os
+
+# Entrada
+numero = (int(input('Qual número que você quer multiplicar?')))
+
+# Processamento
+m1 = numero * 1
+m2 = numero * 2
+m3 = numero * 3
+m4 = numero * 4
+m5 = numero * 5
+m6 = numero * 6
+m7 = numero * 7
+m8 = numero * 8
+m9 = numero * 9
+m10 = numero * 10
+tabuada = m1 , m2 ,m3 ,m4, m5 , m6 , m7 , m8 , m9 , m10
+
+# Saida 
+print('')
+print('-' * 20)
+print(f'Os resultados da tabua de {numero} é, respectivamente: {tabuada} ')
+print('-' * 20)
+print('')
+print('=' * 50)
