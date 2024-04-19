562504c0fe70fefff468c3142fdcd034c3e0ff95
AgataQuadros <quadrosagata@gmail.com>
2024-04-18 21:37:48 -0300

    Exercicio F

---
+++ Atividades/Atividade1/exercicio_f.py
@@ -0,0 +1,30 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO F')
+print('-' * 20)
+
+# Entrada
+numero = (float(input('Digite o numero: ')))
+
+# Processamento
+dobro = numero * 2
+triplo = numero * 3
+
+# Saída
+
+print('-' * 20)
+print('')
+print(f'O drobro de {numero} é {dobro}')
+print(f'enquanto seu tríplo é {triplo}')
+print('')
+print('=' * 50)
