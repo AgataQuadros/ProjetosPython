7eec19ea2d34806fd46d02c9ba3faefd39bce7f6
AgataQuadros <quadrosagata@gmail.com>
2024-04-19 19:30:26 -0300

    Exercício I

---
+++ Atividades/Atividade1/exercicio_i.py
@@ -0,0 +1,29 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 12/04/2024
+
+# Biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO A')
+print('-' * 20)
+
+# Entrada
+real = (float(input('Digite o valor em real: ')))
+
+# Processamento
+valor = real / 5
+
+# Saída
+print('')
+print('-' * 20)
+print(f'Se você tem R${real} então você pode ter ${valor}')
+print('-' * 20)
+print('')
+print('=' * 50)
