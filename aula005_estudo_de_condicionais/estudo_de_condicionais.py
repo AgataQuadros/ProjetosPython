f42da1099864f190d51e2424feae3f9324eecc62
AgataQuadros <quadrosagata@gmail.com>
2024-04-24 21:08:59 -0300

    Condicionais

--- aula005_estudo_de_condicionais/estudo_de_condicionais.py
+++ aula005_estudo_de_condicionais/estudo_de_condicionais.py
@@ -8,3 +8,22 @@ import os
 
 # Limpando o terminal
 os.system('cls')
+
+print('-' * 70)
+print('Estudo de Condicional: Pt1')
+print('-' * 70)
+
+# Entrada
+valor = float(input('digite o numero decimal: '))
+resposta = ''
+
+# Condicional
+if valor % 2 == 0:
+      resposta = f'entrada incorreta, o valor {valor} é um inteiro'
+else:
+      resposta = f'entrada correta, o valor {valor} é um decimal'
+
+# Saida 
+print('=' * 70)
+print(resposta)
+print('fim do programa!\n') # \n salta um linha
