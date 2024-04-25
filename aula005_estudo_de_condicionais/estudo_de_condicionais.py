d0142fc896c9a77ccb8d3b5ac8b312a4f3821b8d
AgataQuadros <quadrosagata@gmail.com>
2024-04-25 19:10:29 -0300

    Condicionais

--- aula005_estudo_de_condicionais/estudo_de_condicionais.py
+++ aula005_estudo_de_condicionais/estudo_de_condicionais.py
@@ -14,15 +14,15 @@ print('Estudo de Condicional: Pt1')
 print('-' * 70)
 
 # Entrada
-valor = float(input('digite o numero decimal: '))
+valor = float(input('digite o numero: '))
 resposta = ''
 
 # Condicional
 if valor % 2 == 0:
       valor = int(valor)
-      resposta = f'entrada incorreta, o valor {valor} é um inteiro'
+      resposta = f'entrada incorreta, o valor {valor} é um par'
 else:
-      resposta = f'entrada correta, o valor {valor} é um decimal'
+      resposta = f'entrada correta, o valor {valor} é um impar'
 
 # Saida 
 print('=' * 70)
