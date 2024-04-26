d75bcbc82150b42f7d1295b0b9732ad55ec0ba78
AgataQuadros <quadrosagata@gmail.com>
2024-04-26 19:09:22 -0300

    Exercício B

---
+++ Atividades/Atividade2/exerciciob.py
@@ -0,0 +1,33 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 25/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO B')
+print('-' * 20)
+
+# Entrada
+a = int(input('digite o primeiro numero: '))
+b = int(input('digite o segundo numero: '))
+c = int(input('digite o terceiro numero: '))
+valor = a,b,c
+resposta = ''
+
+# Processamento
+
+
+
+# Saída
+print('')
+print('-' * 20)
+print(resposta)
+print('-' * 20)
+print('fim do exercício')
+print('=' * 50)
