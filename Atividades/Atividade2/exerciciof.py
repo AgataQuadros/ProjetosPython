3ff72c02b80c5a6403a55643087cbb9abe16af6e
AgataQuadros <quadrosagata@gmail.com>
2024-04-26 21:31:59 -0300

    Exercício F

---
+++ Atividades/Atividade2/exerciciof.py
@@ -0,0 +1,30 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 25/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO E')
+print('-' * 20)
+
+# Entrada 
+ano = int(input('entre com o ano: '))
+resposta = ''
+
+# Processamento
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+
+
+# Saída
+print('')
+print('-' * 20)
+print(resposta)
+print('-' * 20)
+print('fim do exercício')
+print('=' * 50)             
