f6701b5ed685b3641845f4407a34dbdb3ea8d22b
AgataQuadros <quadrosagata@gmail.com>
2024-04-29 18:43:21 -0300

    Exercicio F

--- Atividades/Atividade2/exerciciog.py
+++ Atividades/Atividade2/exerciciog.py
@@ -14,10 +14,12 @@ print('EXERCÍCIO G')
 print('-' * 20)
 
 # Entrada
+a = int(input('entre com o primeiro valor: '))
+b = int(input('entre com o segundo valor: '))
+c = int(input('entre com o terceiro valor: '))
 
-
-
-
-
+# Processamento
+if (a < b + c) and (b < c + a) and (c < b + a):
+    resposta = f'Opa! parece que temos sim um triangulo!'
 
 
