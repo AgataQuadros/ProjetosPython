d000c6332f522f4f5824cfd64e0e1efc69a67196
AgataQuadros <quadrosagata@gmail.com>
2024-04-28 21:22:28 -0300

    Exercício G

---
+++ Atividades/Atividade2/exerciciog.py
@@ -0,0 +1,23 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data 25/04/2024
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO G')
+print('-' * 20)
+
+# Entrada
+
+
+
+
+
+
+
