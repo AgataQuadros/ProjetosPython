7389b1c05dc942c91015e0307c5c20ed15e3c4c5
AgataQuadros <quadrosagata@gmail.com>
2024-04-28 21:44:09 -0300

    Exercício F

--- Atividades/Atividade2/exerciciof.py
+++ Atividades/Atividade2/exerciciof.py
@@ -19,7 +19,9 @@ resposta = ''
 
 # Processamento
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-
+    print(f"{ano} é um ano bissexto.")
+else:
+    print(f"{ano} não é um ano bissexto.")
 
 # Saída
 print('')
