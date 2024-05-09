ab7eb68a062267f9006e96bd37f4eaa8559d445a
AgataQuadros <quadrosagata@gmail.com>
2024-05-09 18:55:30 -0300

    Arredondando Decimal

---
+++ aula006_funcoes_internas/decimais_math.py
@@ -0,0 +1,34 @@
+import math
+import os
+
+
+os.system('cls')
+
+print('-' * 50)
+print('ESTUDO DA BIBLIOTECA MATEMÁTICA MATH')
+print('-' * 50)
+print()
+
+#Entrada de dados
+numero_decimal = float(input('Entre com o número decimal'))
+
+#Processamento
+arredonda_pra_cima = math.ceil(numero_decimal)
+arredonda_pra_baixo = math.floor(numero_decimal)
+
+#Saída
+print('')
+print(f'o numero decimal {numero_decimal} arredondado 
+      para cima é: {numero_decimal}')
+print(f'o numero decimal {numero_decimal} arredondado 
+      para baixo é: {arredonda_pra_baixo}')
+print('-' * 50)
+
+
+
+
+
+
+
+
+
