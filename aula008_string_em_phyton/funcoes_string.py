91aa9538b71d2e35e565fe4df67feca540a156dd
AgataQuadros <quadrosagata@gmail.com>
2024-05-21 19:50:43 -0300

    Função de String

---
+++ aula008_string_em_phyton/funcoes_string.py
@@ -0,0 +1,40 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('Funções String')
+print('-' * 70)
+
+frase1 = 'Ola, Mundo!'
+quantidade_de_caracteres = len(frase1) # conta os characteres
+print(f'A frase {frase1} \n contem {quantidade_de_caracteres} caracteres')
+print('-' * 70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
