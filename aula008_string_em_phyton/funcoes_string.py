a02915e5c886df09251e06a439260d8b03c1b453
AgataQuadros <quadrosagata@gmail.com>
2024-05-21 19:56:34 -0300

    Função String

--- aula008_string_em_phyton/funcoes_string.py
+++ aula008_string_em_phyton/funcoes_string.py
@@ -12,15 +12,15 @@ quantidade_de_caracteres = len(frase1) # conta os characteres
 print(f'A frase {frase1} \n contem {quantidade_de_caracteres} caracteres')
 print('-' * 70)
 
+minunculas = frase1.lower() # frase em minúsculo
+print(f'Frase original: {frase1}')
+print(f'Frase nova: {minunculas}')
+print('-' * 70)
 
-
-
-
-
-
-
-
-
+maiuscula = frase1.capitalize() # frase capitalizada/maiuscula
+print(f'Frase original: {frase1}')
+print(f'Frase nova: {maiuscula}')
+print('-' * 70)
 
 
 
