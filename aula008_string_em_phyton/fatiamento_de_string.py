677eedd5aa4b5e53b6c75c1b700f7e1cb5e745e5
AgataQuadros <quadrosagata@gmail.com>
2024-05-21 19:24:12 -0300

    Aula 008

---
+++ aula008_string_em_phyton/fatiamento_de_string.py
@@ -0,0 +1,11 @@
+#biblioteca
+import os
+
+
+os.system('cls')
+
+frase = 'String em Python'
+texto = "String em Python"
+
+print(f'imprimindo a variavel "frase": {frase}')
+print(f'imprimindo a variavel "frase": {frase}\n')
