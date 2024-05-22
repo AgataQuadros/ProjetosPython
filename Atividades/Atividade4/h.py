a1ac0ff0b460703a12479721e647c9101b0ca88a
AgataQuadros <quadrosagata@gmail.com>
2024-05-21 21:40:40 -0300

    Exercicio H

--- Atividades/Atividade4/h.py
+++ Atividades/Atividade4/h.py
@@ -17,3 +17,20 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO H')
 print('-' * 20)
+
+#entrada
+nome = str(input('Entre com o seu nome: '))
+resposta = ''
+
+#prcessamento
+numeros_de_letras = nome.count(O)
+
+resposta = (f'O seu nome contem {numeros_de_letras} letras!')
+
+
+print('')
+print('-' * 20)
+print(resposta)
+print('-' * 20)
+print('fim do exercício :D')
+print('=' * 50)
