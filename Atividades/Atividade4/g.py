60838b1a1a3ca3b66bf932f9c26413344cd3c364
AgataQuadros <quadrosagata@gmail.com>
2024-05-23 18:38:09 -0300

    Exercício G

--- Atividades/Atividade4/g.py
+++ Atividades/Atividade4/g.py
@@ -12,6 +12,7 @@
 import os
 import math
 
+
 # Limpando o terminal
 os.system('cls')
 
@@ -19,18 +20,23 @@ print('=' * 50)
 print('EXERCÍCIO G')
 print('-' * 20)
 
+#Entrada
 numero = int(input('Entre com um numero inteiro: '))
 
+#processamento
+unidade = numero % 10
+dezena = (numero // 10) % 10
+centena = (numero // 100) % 10
+milhar = (numero // 1000) % 10
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Saida
+print('')
+print('-' * 20)
+print(f'O seu numero {numero} tem:')
+print(f'{unidade} unidades')
+print(f'{dezena} dezenas')
+print(f'{centena} centena')
+print(f'{milhar} unidades de milhar')
+print('-' * 20)
+print('fim do exercício :D')
+print('=' * 50)
