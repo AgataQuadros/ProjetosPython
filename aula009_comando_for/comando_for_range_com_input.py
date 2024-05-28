00ac75063c1fb4ef93a409090973a75232fc188c
AgataQuadros <quadrosagata@gmail.com>
2024-05-28 18:27:00 -0300

    Estrutura de controle FOR RANGE

---
+++ aula009_comando_for/comando_for_range_com_input.py
@@ -0,0 +1,16 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTUTURA DE CONTROLE FOR RANGE')
+print('=' * 70)
+
+print()
+
+for var_iteradora in range(1, 5) :
+    cor = str(input(f'Digite a {var_iteradora}ª cor: '))
+
+print()
+print()
