e6d5709f4170a52d58c5220cd18dca0a5370751c
AgataQuadros <quadrosagata@gmail.com>
2024-05-28 21:03:33 -0300

    Comando WHILE

---
+++ aula010_comando_while/comando_while.py
@@ -0,0 +1,19 @@
+import os
+
+
+os.system('cls')
+
+
+print('-' * 70)
+print('COMANDO WHILE')
+print('-' * 70)
+
+contador = 1
+
+while contador <= 10 :
+    print(f'contando... {contador}')
+
+    # contador += 1 <-> contador = contador + 1
+    contador += 1
+
+print('Fim do programa!\n')
