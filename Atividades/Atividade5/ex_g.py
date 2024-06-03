0b5e0ef263e4540650f41daa4a65c2e7e6b1281a
AgataQuadros <quadrosagata@gmail.com>
2024-06-03 20:37:28 -0300

    Exercício G

--- Atividades/Atividade5/ex_g.py
+++ Atividades/Atividade5/ex_g.py
@@ -18,17 +18,17 @@ print('=' * 50)
 print('EXERCÍCIO G')
 print('-' * 20)
 
+# Entrada
+inicio = int(input('Entre com o inicio do intervalo: '))
+fim = int(input('Entre com o final do intervalo: '))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Processamento
+if inicio < 2:
+   print(f'Não existe numero primo de {inicio}, tente outro numero')
+else:
+   for c in range((inicio),(fim)):
+      for m in range(inicio,c):
+         if c % m == 0:
+            break
+      else:
+         print(c, end=' - ')
