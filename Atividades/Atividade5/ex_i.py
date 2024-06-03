bf0ef9735da0ccbcce6c070a0cfec9e69459c3bf
AgataQuadros <quadrosagata@gmail.com>
2024-06-03 18:59:16 -0300

    Exercício I

--- Atividades/Atividade5/ex_i.py
+++ Atividades/Atividade5/ex_i.py
@@ -22,16 +22,23 @@ print('=' * 50)
 print('EXERCÍCIO I')
 print('-' * 20)
 
+# Entrada
+print('Eu estou em loop!')
+print('')
 
+# Processamento
+while (True):
+    frase2 = input('Digite F para sair do Loop!!! : ')
 
+    if 'f' not in frase2:
+        print('')
+        print('Digite f...')
+    else:
+        print('-' * 20)
+        print('Fim do Loop!!Muito Obrigado!!')
+        break
 
-
-
-
-
-
-
-
-
-
-
+# Saída
+print('')
+print('-' * 20)
+print('=' * 50)
