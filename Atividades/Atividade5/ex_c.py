1ba8fb00c4304137ea8e0cebb01ab7927850c6da
AgataQuadros <quadrosagata@gmail.com>
2024-06-04 18:40:10 -0300

    Atividade C

---
+++ Atividades/Atividade5/ex_c.py
@@ -0,0 +1,26 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Objetivo: Faça um programa que imprima os 
+# números no intervalo entre 1 e 10 em ordem inversa.
+
+# Biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO C')
+print('-' * 20)
+
+# Processamento
+for c in range(10, 0, -1):
+    print('----')
+    print(f'{c}')
+
+# Saida
+print('-' * 20)
+print('Fim do exercício :D')
+print('=' * 50)
