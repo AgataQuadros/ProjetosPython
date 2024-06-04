f6682d7563176d8792884ae734573a0b674b3d46
AgataQuadros <quadrosagata@gmail.com>
2024-06-04 18:19:18 -0300

    Atividade 5

---
+++ Atividades/Atividade5/ex_b.py
@@ -0,0 +1,23 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Objetivo: Evolua o programa anterior para um código que 
+# pergunte ao usuário qual o intervalo 
+# que ele deseja ver  impresso.
+
+# Biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO B')
+print('-' * 20)
+
+
+
+
+
+
