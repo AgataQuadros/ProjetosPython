f5bbf103d347fd77fc732fddf173d12eb33d3da0
AgataQuadros <quadrosagata@gmail.com>
2024-06-04 18:16:13 -0300

    Exercício A

---
+++ Atividades/Atividade5/ex_a.py
@@ -0,0 +1,30 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Objetivo: Faça um programa que imprima os números no 
+# intervalo entre 1 e 100. 
+# Os números devem ser apresentados na horizontal.
+
+# ENVIAR
+
+# Biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO A')
+print('-' * 20)
+
+# Processamento
+for c in range(1,100):
+    print('----')
+    print(c)
+
+# Saida
+print('')
+print('-' * 20)
+print('Fim do exercício :D')
+print('=' * 50)
