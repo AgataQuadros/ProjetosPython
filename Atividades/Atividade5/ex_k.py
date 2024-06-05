54d4f76d459ce90b1f58df4a268552499371305c
AgataQuadros <quadrosagata@gmail.com>
2024-06-05 19:10:43 -0300

    Exercício K

---
+++ Atividades/Atividade5/ex_k.py
@@ -0,0 +1,17 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Objetivo: Crie um programa que pede que o usuário digite um nome ou uma frase, 
+# verifique se esse conteúdo digitado é um palíndromo ou não, 
+# exibindo em tela esse resultado.
+
+# Biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('=' * 50)
+print('EXERCÍCIO K')
+print('-' * 20)
