a67037157e33db6a42a844ae830bd489a4dceb9e
AgataQuadros <quadrosagata@gmail.com>
2024-06-12 21:32:19 -0300

    Desafio

---
+++ exercicios/desafio.py
@@ -0,0 +1,12 @@
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Desafio: Construa um código para exemplificar 
+# um CRUD. Não é permitido 
+# funções ou validações try exception.
+
+import os
+
+
+
+
+
