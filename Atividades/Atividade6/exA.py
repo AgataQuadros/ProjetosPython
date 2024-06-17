524bfa076149a55b8f15b8ad3ca45c05362e4882
AgataQuadros <quadrosagata@gmail.com>
2024-06-17 19:33:18 -0300

    Execício A

--- Atividades/Atividade6/exA.py
+++ Atividades/Atividade6/exA.py
@@ -15,3 +15,18 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO A')
 print('-' * 20)
+
+
+# Entrada
+lista = [1, 2, 3, 5, 6]
+
+print(f'A lista {lista} esta faltando um número')
+numero = int(input('Qual número esta faltando?(digite apenas o número): '))
+posicao = int(input('Em qual psição ele se encaixa?(digite apenas o número): '))
+
+# Processamento
+if posicao == 3  and numero == 4:
+    lista.insert(posicao, numero)
+    print()
+    print('Parabens!! você corrigiu a lista!!')
+    print('Lista após a correção:', lista)
