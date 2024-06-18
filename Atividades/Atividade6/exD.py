b0e403694433fb3900261bb7e6ea537c65544cd6
AgataQuadros <quadrosagata@gmail.com>
2024-06-18 19:26:04 -0300

    Exercício D

--- Atividades/Atividade6/exD.py
+++ Atividades/Atividade6/exD.py
@@ -16,3 +16,15 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO D')
 print('-' * 20)
+
+# Funções
+lista_aux = []
+
+# Entrada
+nota_1 = input('Entre com a nota do 1° aluno:')
+nota_2 = input('Entre com a nota do 2° aluno:')
+nota_3 = input('Entre com a nota do 3° aluno:')
+nota_4 = input('Entre com a nota do 4° aluno:')
+
+notas = nota_1,nota_2,nota_3,nota_4
+lista_aux = notas.split(notas)
