a67c00cdbb0cc2d8d498d331376903b77de5b3ce
AgataQuadros <quadrosagata@gmail.com>
2024-06-19 21:07:37 -0300

    Exercício A

--- Atividades/Atividade7/EXa.py
+++ Atividades/Atividade7/EXa.py
@@ -5,6 +5,8 @@
 # número indeterminado de notas 
 # (pressione ‘s’ ou ‘0’ para sair).
 
+
+
 # Biblioteca
 import os
 
@@ -15,3 +17,16 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO A')
 print('-' * 20)
+
+# Declaração
+lista_aux = []
+notas = []
+soma = 0
+media = 0
+
+# entrada
+for i in notas:
+    if 's' or 0 != notas:
+        notas.append(int(input(f' Entre com a nota do {i}° aluno: ')))
+    else:
+        print('Fim da datação de notas')
