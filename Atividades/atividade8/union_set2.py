3a1a59a72bb4cecde745ac6d5ed8133036f8fc6b
AgataQuadros <quadrosagata@gmail.com>
2024-06-24 20:45:19 -0300

    Métodos para Set

---
+++ Atividades/atividade8/union_set2.py
@@ -0,0 +1,22 @@
+# Trabalho sobre a estrutura de dados SET
+# Senac MG/JF
+# Turma 0152
+# Autor: Agata Quadros (Gaia)
+# Data: 24/06/2024
+
+# Os alunos farão, por sorteio, um programa (1 por aluno) utilizando os 
+# métodos da estrutura de dados Set . 
+# Depois de prontos, todos os exemplos gerados pelos alunos farão parte da apostila 
+# de linguagem de programação python do Curso de Desenvolvimento de Sistemas. 
+# Atenção para colocarem uma descrição do programa, sua data de criação,
+# o ano e a turma. Como temos 14 alunos, 4 trabalhos serão feitos em dupla.
+# Hora de caprichar nas ideias.
+
+# union(set2) ou |: Retorna um novo set que é a união de dois sets.
+
+
+# Biblioteca
+import os
+
+# Limpando o terminal
+os.system('cls')
