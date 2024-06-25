4f0d2642f3a6755d916982d85466a454c4f05eab
AgataQuadros <quadrosagata@gmail.com>
2024-06-25 19:59:09 -0300

    Dicionário

---
+++ aula014_dicionario/dicionario.py
@@ -0,0 +1,33 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE DADOS: DICIONÁRIOS')
+print('=' * 70)
+
+
+compras = {}
+pessoas = {}
+cores = {}
+elementos = dict()
+numeros = dict()
+
+# Atribuindo
+compras['id'] = 1
+compras['item'] = 'caderno'
+compras['valor'] = 10.80
+
+pessoas['id'] = '0010'
+pessoas['nome'] = 'Sherlock Holmes'
+pessoas['endereço'] = 'Baker Street'
+pessoas['numero'] = '221B'
+pessoas['cidade'] = 'Londres'
+pessoas['pais'] = 'inglaterra'
+
+cores['Pb'] = 'Vermelho'
+cores['Au'] = 'Verde'
+cores['N'] = 'Azul'
+
+
