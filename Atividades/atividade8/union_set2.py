2bf0079afd5739619a663b7e1a8ba20f39e5a8ed
AgataQuadros <quadrosagata@gmail.com>
2024-06-25 19:26:20 -0300

    Union

--- Atividades/atividade8/union_set2.py
+++ Atividades/atividade8/union_set2.py
@@ -33,4 +33,20 @@ entrada1 = input('Entre com a 1ª lista que você desa unir separando cada eleme
 entrada2 = input('Entre com a 2ª lista que você desa unir separando cada elemento por ",": ')
 
 # processamento
-numero_str = entrada1.split()
+# tranformando as entradas em listas
+lista1 = entrada1.split(',')
+lista2 = entrada2.split(',')
+# Transformando em set
+set1 = set(lista1)
+set2 = set(lista2)
+# unindo as duas lista
+#unidas = set1.union(set2)
+
+# corrigindo a sequencia com sort
+#if int in entrada1 or entrada2:
+#    sequencia = unidas.sort 
+
+print(lista1)
+print(set1)
+print(lista2)
+print(set2)
