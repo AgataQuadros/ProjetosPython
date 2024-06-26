d58a383dfd1eb0d88e93891b3bc8215c47df4db0
AgataQuadros <quadrosagata@gmail.com>
2024-06-26 20:27:17 -0300

    Métodos Items(), Keys() e Values()

--- aula014_dicionario/metodos3.py
+++ aula014_dicionario/metodos3.py
@@ -19,25 +19,37 @@ while True:
     opcao = input('escolha entre as opções: ')
 
     if opcao == '1':
-        # Adicionar um novo par-chave do usuario
+        # Adiciona um novo par-chave ao dicionario
         chave = input('Digite a chave: ')
         valor = input('Digite o valor: ')
         meu_dicionario[chave] = valor
         print(f'Par {chave}:{valor} adicionados')
     
     elif opcao == '2':
-        # Exibe o dicionario atual
-        print('Dicionário atual: ', meu_dicionario)
+        # mostra as chaves do dicionario
+        if meu_dicionario:
+            print('Chaves do dicionário:', meu_dicionario.keys())
+        else:
+            print('O dicionário esta vazio. Adicione itens primeiro')
 
     elif opcao == '3':
-        # Mostrar o tamanho do dicionario usando len
-        tamanho = len(meu_dicionario)
-        print(f'O dicionário tem {tamanho} elementos')
+        # Mostrar o valor do dicionario usando values()
+        if meu_dicionario:
+            print('Valores do dicionário: ', meu_dicionario.values())
+        else:
+            print('O dicionário esta vazio. Adicione itens primeiro')
 
     elif opcao == '4':
-        # Cria uma copia do dicionario usando copy() e exibe a copia
-        copia_dicionario = meu_dicionario.copy()
-        print('Copia do dicionario: ', copia_dicionario)
+        # mostra os itens(chave-valor) do dicionario usando itens()
+        if meu_dicionario:
+            print('Itens do dicionário: ', meu_dicionario.items())
+        else:
+            print('O dicionário esta vazio. Adicione itens primeiro')
+
 
     elif opcao == '5':
+        print('Saindo do programa...')
         break
+    else:
+        # mensagem de operação invalida
+        print('Opção invalida. Tente novamente')
