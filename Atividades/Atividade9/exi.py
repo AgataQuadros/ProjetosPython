1e30dd03ef22e65574d0a9cb4ef00f8602b7f4ee
AgataQuadros <quadrosagata@gmail.com>
2024-06-28 20:41:28 -0300

    Exercício I

--- Atividades/Atividade9/exi.py
+++ Atividades/Atividade9/exi.py
@@ -15,3 +15,39 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO I')
 print('-' * 20)
+
+# Declarado
+usuario = {}
+
+# Entrada
+print()
+print('Procurando usuario...')
+print()
+entrada = input('Usuario encontrado. Aperte enter para prosseguir...')
+
+# Atribuindo
+usuario['nome'] = 'Mônica Oliveira'
+usuario['titulo'] = 'Dona da rua'
+usuario['endereço'] = 'Bairro do limoeiro'
+usuario['numero'] = '22'
+
+# Processamento
+if entrada == '':
+    print(usuario)
+    excluir = input('Deseja excluir a ultima informação do perfil? (s/n)')
+    if excluir == 's':
+        print()
+        valor_removido = usuario.popitem()
+
+else:
+    print('Saindo do painel de entrada.')
+
+# Saida
+print()
+print('Informação retirada.')
+print('Perfil atualizado:')
+print(usuario)
+print()
+print('-' * 20)
+print('Fim do programa')
+print('=' * 50)
