7b069fdf7765b0a3bb82cf507c28e6411be914c8
AgataQuadros <quadrosagata@gmail.com>
2024-06-28 19:57:31 -0300

    Exercicios

--- Atividades/Atividade9/exa.py
+++ Atividades/Atividade9/exa.py
@@ -13,3 +13,27 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO A')
 print('-' * 20)
+
+# Declarado
+usuario = {}
+# Entrada
+print()
+print('Procurando usuario...')
+print()
+entrada = input('Usuario encontrado. Aperte enter para prosseguir...')
+
+# Atribuindo
+usuario['nome'] = 'Mônica Oliveira'
+usuario['titulo'] = 'Dona da rua'
+usuario['endereço'] = 'Bairro do limoeiro'
+usuario['numero'] = '22'
+
+# Saida
+if entrada == '':
+    print(usuario)
+else:
+    print('Saindo do painel de entrada.')
+
+print('-' * 20)
+print('Fim do programa')
+print('=' * 50)
--- Atividades/Atividade9/exb.py
+++ Atividades/Atividade9/exb.py
@@ -14,3 +14,41 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO B')
 print('-' * 20)
+
+# Declarado
+usuario = {}
+
+# Entrada
+print()
+print('Procurando usuario...')
+print()
+entrada = input('Usuario encontrado. Aperte enter para prosseguir...')
+
+# Atribuindo
+usuario['nome'] = 'Mônica Oliveira'
+usuario['titulo'] = 'Dona da rua'
+usuario['endereço'] = 'Bairro do limoeiro'
+usuario['numero'] = '22'
+
+# Processamento
+if entrada == '':
+    print(usuario)
+    adicionar = input('Deseja adicionar mais informações ao perfil? (s/n)')
+    if adicionar == 's':
+        for i in range(2):
+            nome = str(input('Entre com o nome da informação: '))
+            informacao = str(input('Entre com a informação: '))
+            usuario[nome] = informacao
+            
+else:
+    print('Saindo do painel de entrada.')
+
+# Saida
+print()
+print('Informações adicionadas')
+print('Perfil atualizado:')
+print(usuario)
+print()
+print('-' * 20)
+print('Fim do programa')
+print('=' * 50)
