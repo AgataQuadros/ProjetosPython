882390e8d191ca9249de7f4fb4f907fcb0378996
AgataQuadros <quadrosagata@gmail.com>
2024-07-11 21:35:58 -0300

    Termina

--- Atividades/Atividade10/b.py
+++ Atividades/Atividade10/b.py
@@ -1,7 +1,7 @@
 # Curso de Desenvolvimento de Sistemas
 # Turma 0152
 # Autor: Agata Quadros (Gaia)
-# Objetivo: Crie uma função que cadastre o nome de uma aluno,
+# Objetivo: Crie uma função que cadastre o nome de um aluno,
 #  a matrícula e a data de nascimento.
 # Depois imprima os resultados cadastrados utilizando uma estrutura de repetição for.
 
@@ -15,3 +15,21 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO B')
 print('-' * 20)
+
+cadastro = []
+aluno = {}
+
+def cadastrar():
+    aluno['nome'] = input('Insira o nome do aluno: ')
+    aluno['matricula'] = input('Insira o nome do matricula: ')
+    aluno['nascimento'] = input('Insira o nome do data de nascimento: ')
+    
+    os.system('cls')
+    cadastro.append(aluno.copy())
+
+while True:
+    cadastrar()
+    for info in cadastro:
+        print(info)
+        if 'sair' in aluno:
+            break
