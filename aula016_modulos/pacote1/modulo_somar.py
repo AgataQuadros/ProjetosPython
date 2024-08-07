d1cc43baa76524b208918e32a636c554d1523228
AgataQuadros <quadrosagata@gmail.com>
2024-08-07 18:31:19 -0300

    Modulo Soma

---
+++ aula016_modulos/pacote1/modulo_somar.py
@@ -0,0 +1,12 @@
+""""Método para somar 2 números
+        
+    Args:
+        a (any): Percela 1
+        b (any): Parcela 2
+    
+    Returns:
+        Any: Soma das duas parcelas
+    """
+def somar(a, b):
+    soma = a +b
+    return soma
