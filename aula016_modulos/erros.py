4c7da3b5fb9eae5b2a252e0eefb40b7601af232b
AgataQuadros <quadrosagata@gmail.com>
2024-08-07 21:13:09 -0300

    Erros

---
+++ aula016_modulos/erros.py
@@ -0,0 +1,62 @@
+# ZeroDivisionError
+try:
+    resultado = 10 / 0
+except ZeroDivisionError:
+    print('Erro: Divisão por zero!')
+
+# ValueError
+try:
+    soma = '5' + 5
+except TypeError:
+    print('Erro: Tipo de dado imcompatível!')
+
+# IndexError
+lista = [1, 2, 3]
+try:
+    item = lista[5]
+except IndexError:
+    print('Erro: tipo de dado imcompativel!')
+
+# KeyError
+dicionario = {'chave' : 'valor'}
+try:
+    valor = dicionario['claro_inexistente']
+except KeyError:
+    print('Erro: Chave não encontrada no dicionário!')
+
+# FileNotFoundError
+try:
+    with open('arquivo_inexistente.txt', 'r') as arquivo:
+        conteudo = arquivo.read()
+except FileNotFoundError:
+    print('Erro: Arquivo não encontrado!')
+
+# IOError
+try:
+    with open('arquivo.txt', 'w') as arquivo:
+        conteudo = arquivo.read()
+except IOError:
+    print('Erro: Operação de E/S falhou!')
+
+# AttributeError
+class Exemplo:
+    def __init__(self):
+        self.atributo = 'valor'
+
+objeto = Exemplo()
+try:
+    print(objeto.atributo_inexistente)
+except AttributeError:
+    print('Erro: Atributo não encontrado no objeto!')
+
+# ImportError
+try:
+    import modulo_inexistente # type: ignore - ignora o erro
+except ImportError:
+    print('Erro: Módulo não encontrado!')
+
+# RuntimeError
+try:
+    raise RuntimeError('Este é um erro de execução!')
+except RuntimeError as e:
+    print(f'Erro: {e}')
