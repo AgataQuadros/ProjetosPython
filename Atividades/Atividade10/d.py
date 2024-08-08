a7b46e93c1c3ff703bdb53a3bb8902936d3c50bc
AgataQuadros <quadrosagata@gmail.com>
2024-08-08 20:52:57 -0300

    Exercício D

--- Atividades/Atividade10/d.py
+++ Atividades/Atividade10/d.py
@@ -14,3 +14,14 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO D')
 print('-' * 20)
+
+
+def fahre_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+
+entrada_fahrenheit = int(input('entre com o grau em fahrenheit: '))
+c = fahre_celsius(entrada_fahrenheit)
+
+print(f'\nA temperatura {entrada_fahrenheit}ºF em Celsius é {c}ºC')
